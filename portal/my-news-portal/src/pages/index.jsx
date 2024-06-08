7594fb0a137bab0009fdc7db087b401f0c022e8b
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-08 09:15:26 -0300

    Recuperando Noticias

--- portal/my-news-portal/src/pages/index.jsx
+++ portal/my-news-portal/src/pages/index.jsx
@@ -3,6 +3,17 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const fetchArticles = async () => {
+            const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`);
+            const data = await res.json();
+            setArticles(data.articles);
+        };
+        fetchArticles();
+    }, []);
+
     return (
         <div>
             <h1>Latest News</h1>
