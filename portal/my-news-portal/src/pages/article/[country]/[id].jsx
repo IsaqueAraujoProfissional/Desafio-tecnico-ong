1a7e77d26ad61523618cad9ac0700b51083434ff
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-08 16:35:10 -0300

    Redução de código, utilizando fallback

--- portal/my-news-portal/src/pages/article/[country]/[id].jsx
+++ portal/my-news-portal/src/pages/article/[country]/[id].jsx
@@ -11,7 +11,7 @@ export default function Article({ article }) {
     if (router.isFallback) {
         return <div>Loading...</div>;
     }
-    
+
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>{article.title}</h1>
@@ -28,35 +28,10 @@ export default function Article({ article }) {
 }
 
 export async function getStaticPaths() {
-    const countries = ['us', 'br'];
-    const paths = [];
-
-    for (const country of countries) {
-        const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`);
-        //const data = await res.json();
-        const data = {
-            "status": "ok",
-            "totalResults": 45,
-            "articles": [
-                {
-                    "source": {
-                        "id": "bloomberg",
-                        "name": "Bloomberg"
-                    },
-                    "author": "Matthew Martin, Julia Fioretti",
-                    "title": "Saudis Said to Hand About 60% of Aramco Offer to Foreign Funds - Bloomberg",
-                    "description": "Foreign investors are set to be allocated about 60% of the shares on offer in Saudi Aramco’s $11.2 billion stock sale, people familiar with the matter said, marking a turnaround from the oil giant’s 2019 listing that ended up as a largely local affair.",
-                    "url": "https://www.bloomberg.com/news/articles/2024-06-08/saudis-said-to-hand-about-60-of-aramco-offer-to-foreign-funds",
-                    "urlToImage": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJtGT7mWJI_Q/v0/1200x800.jpg",
-                    "publishedAt": "2024-06-08T13:04:37Z",
-                    "content": "Foreign investors are set to be allocated about 60% of the shares on offer in Saudi Aramcos $11.2 billion stock sale, people familiar with the matter said, marking a turnaround from the oil giants 20… [+332 chars]"
-                }
-            ]
-        }
-        paths.push(...data.articles.map((_, index) => ({ params: { country, id: index.toString() } })));
-    }
-
-    return { paths, fallback: true };
+    return {
+        paths: [], // Vamos usar fallback para carregar dinamicamente as páginas
+        fallback: 'blocking'
+    };
 }
 
 export async function getStaticProps({ params }) {
@@ -83,6 +58,12 @@ export async function getStaticProps({ params }) {
         ]
     }
     const article = data.articles[id];
+
+    if (!article) {
+        return {
+            notFound: true,
+        };
+    }
 
     return { props: { article } };
 }
